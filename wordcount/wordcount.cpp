4cdb766a5d6897c9bac6d6f14464e8c05729a57b
LuoAiyue <55022917+LuoAiyue@users.noreply.github.com>
2019-10-19 11:29:09 +0800

    Update wordcount.cpp

--- wordcount/wordcount.cpp
+++ wordcount/wordcount.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
-int main(int argc,char argv[]){
-	
+void output()
+{
+    int count=2;
+    int num=1;
+    string l[100]={"22222","mmmmm"};
+    cout <<"characters: "<<characters_sum()<<endl;  //字符数
+    cout<<endl;
+    cout << "words: "<<WordsCount()<<endl;    //单词个数
+    cout<<endl;
+    cout << "lines: "<<lineCount()<<endl;//行输出
+    cout<<endl;
+    for (int i = 0; i < count; i++)
+    {
+        cout << "<" << l[i] << ">:" <<" "<<num<<endl;
+    }
+}
+int main(int argc,char argv[])
+{
+	output();
 	return 0;
 }
